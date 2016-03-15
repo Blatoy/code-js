9d0c258263006b3dc2d975d276cb656f7a76175e
Jeanneret Loïck <lock.jeanneret@gmail.com>
2016-03-15 16:44:51 +0100

    Added project listing

--- client/resources/logic/core/project-manager.js
+++ client/resources/logic/core/project-manager.js
@@ -10,7 +10,15 @@ var projectManager = function() {
 			$("#file-content").text("There's nothing here.");
 		}
 		else {
-			$("#file-content").html("<div class='file'>"+data[0].projectId+"</div>");
+			for(var i = 0; i++; i < data.length) {
+				var imageType = "folder";
+				$("#file-content").html('<div class="file">' + 
+					'<span class="file-icon"><img src="resources/ui/images/folder.png" alt="' + folder + '-image"/></span>' + 
+					'<span class="file-title">' + data.projectName + '</span>' + 
+					'<span class="file-last-modification">' + data.lastModification + '</span>' + 
+					'<span class="file-working-people">' + Math.round(Math.random() * 20) + '</span>' + 
+				'</div>');
+			}
 		}
 	};
 	
